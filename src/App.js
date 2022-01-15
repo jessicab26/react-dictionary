ae5eefb5f773d1bbc87fef55ed4d27fcb80df195
jessicab26 <brogdenjessica@gmail.com>
2022-01-15 14:22:18 -0600

    add css gradient background to app.js file/added header to app.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Dictionary</h1>
+        <header>Dictionary</header>
         <Search />
       </div>
     </div>
