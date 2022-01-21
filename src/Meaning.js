9149fbecfbbbc9c1183d97c02aa920aea7e5b246
jessicab26 <brogdenjessica@gmail.com>
2022-01-21 08:37:39 -0600

    added .map function to Synonyms.js to loop through individual synonyms for each definition/imported definition.css file to meaning.js and synonyms.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,13 +1,29 @@
 import React from "react";
+import Synonyms from "./Synonyms";
+import "./Definition.css";
 
 export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      <p>{props.meaning.definitions[0]}</p>
-      <p>
-        <strong>Sample sentence:</strong> {props.meaning.definitions[0].example}
-      </p>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>{definition.definition}</p>
+            <span>
+              <strong>Sample Sentence:</strong>
+            </span>{" "}
+            <span className="sentence">
+              <em>"{definition.example}"</em>
+            </span>
+            <br />
+            <p>
+              <strong>Synonyms:</strong>
+            </p>
+            <Synonyms synonyms={definition.synonyms} />
+          </div>
+        );
+      })}
     </div>
   );
 }
